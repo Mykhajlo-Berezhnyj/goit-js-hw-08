2133c68144a71d3f7be1c96970142435a29c817d
Mykhajlo <bmve@ukr.net>
2025-02-20 16:57:00 +0200

    add navigation

--- js/gallery.js
+++ js/gallery.js
@@ -64,34 +64,82 @@ const images = [
   },
 ];
 
-const gallery = document.querySelector(".gallery");
+
+const gallery = document.querySelector('.gallery');
 const galleryItems = images.map(({ preview, original, description }) => {
-  return ` <li class="gallery-item">
-    <a class="gallery-link" href="${original}">
-      <img
-        class="gallery-image"
-        src="${preview}"
-        data-source="${original}"
-        alt="${description}"
-      />
-    </a>
-  </li>`;
+  return `<li class="gallery-item">
+  <a class="gallery-link" href="${original}">
+    <img
+      class="gallery-image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
 }).join('');
 
 gallery.innerHTML = galleryItems;
 
-gallery.addEventListener('click', clickGallery);
-function clickGallery(event) {
+
+let instance = null;
+let currentIndex = 0;
+
+gallery.addEventListener('click', clickOpenModal);
+
+function clickOpenModal(event) {
   event.preventDefault();
-  if (!event.target.classList.contains('gallery-image')) {
+  if (event.target.nodeName !== 'IMG') {
     return;
   }
   const imgUrl = event.target.dataset.source;
+  currentIndex = images.findIndex(images => images.original === imgUrl);
+  console.log("🚀 ~ clickOpenModal ~ currentIndex:", currentIndex)
+  
+  console.log("🚀 ~ clickOpenModal ~ currentIndex:", currentIndex)
+    instance = basicLightbox.create(
+      `<img src="${imgUrl}" width = '1112px' height = '640px'>`, {
+      onShow: () => {
+        document.addEventListener('keydown', navigationModal);
+      },
+      onClose: () => {
+        document.removeEventListener('keydown', navigationModal);
+        instance = null;
+      },
+    });
+    instance.show();
+ }
 
-      const instance = basicLightbox.create(
-    `<img src="${imgUrl}">`
-)
-  instance.show();
-};
+
+function navigationGallery(event) {
+   
+ }
+
+function navigationModal(event) {
+  if (!instance) {
+    return;
+  }
+  event.preventDefault();
+  console.log("🚀 ~ navigationModal ~ event.key:", event.key)
+  if (event.key === '5' || event.key === ' '|| event.key === 'Escape' || event.key === "Enter" ) {
+    instance.close();
+    return;
+  }
+  if ((event.key === 'ArrowLeft' ||event.key === 'ArrowDown' || event.key === 'A'|| event.key === 'a' || event.key === '4'|| event.key === "numpad4" ) ) {
+    currentIndex = (currentIndex + 1 + images.length) % images.length;
+    updateModalImage();
+  }
+  if ((event.key === 'ArrowRight' || event.key === 'ArrowUp' || event.key === '6'|| event.key === 'd' || event.key === 'D'|| event.key === "numpad6" ) ) {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    updateModalImage();
+  }
+}
+
+function updateModalImage() {
+  const newImgUrl = images[currentIndex].original;
+  const modalImg = instance.element().querySelector('img');
+  modalImg.src = newImgUrl;
+
+}
 
 
